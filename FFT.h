79bc6b8ebc6743d205cd0341eeef67fdc45834ac
EduardKhromtsov <109735904+EduardKhromtsov@users.noreply.github.com>
2023-05-25 19:51:45 +0300

    Update FFT.h

--- FFT.h
+++ FFT.h
@@ -3,13 +3,13 @@
 
 #include <vector>
 #include <complex>
-#define _USE_MATH_DEFINES // Для получения числа Пи
+#define _USE_MATH_DEFINES // Г„Г«Гї ГЇГ®Г«ГіГ·ГҐГ­ГЁГї Г·ГЁГ±Г«Г  ГЏГЁ
 #include <math.h>
 
 class FFT
 {
 public:
-	static void fft(std::vector<std::complex<double>>& refComplexNumbers, const bool invert); // Параметр invert показывает, прямое или обратное ДПФ следует вычислить
+	static void fft(std::vector<std::complex<double>>& refComplexNumbers, const bool invert); 
 };
 
 #endif
